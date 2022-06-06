10cc5e7cc20084af2317c2c86de9097913e819f0
hainh-0199 <44605732+hainh184088@users.noreply.github.com>
2022-06-06 05:11:33 +0000

    Change Todo status

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,13 +4,18 @@
 　・チェックボックスにチェックが入っているか管理する
 　・チェックボックスにチェックが入っているかアイテムをグレーアウトする
 */
-function TodoItem({item}  ) {
+
+function TodoItem({ item }) {
+  var name = {item.text};
   return (
     <label className="panel-block">
-       <input
+      <input
         type="checkbox"
-         />
+        onClick={document.querySelector(`.${item.text}`) ? 'has-text-grey-light' : ''}
+      />
+      <span className={{name}}
         {item.text}
+      </span>
     </label>
   );
 }
